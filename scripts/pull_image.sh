8fa70abbe95993b60c65bccb043e04f263d1f370
Licheng Nie <nielicheng@gmail.com>
2016-03-10 16:57:52 +1100

    Change shell

--- scripts/pull_image.sh
+++ scripts/pull_image.sh
@@ -2,4 +2,8 @@
 
 set -e
 
-docker run -d --name ElevatorApplication -p 8081:8081 elevatorapplication:latest
+#Login to ECR Repository
+LOGIN_STRING=`aws ecr get-login`
+${LOGIN_STRING}
+
+docker pull 234539630134.dkr.ecr.us-east-1.amazonaws.com/elevatorapplication:latest
--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-docker run -d --name ElevatorApplication -p 8081:8081 elevatorapplication
+docker run -d --name ElevatorApplication -p 8081:8081 234539630134.dkr.ecr.us-east-1.amazonaws.com/elevatorapplication:latest
